d96869749b425c7d38b93336016642dbbcf5d914
jyko2658 <87454284+jyko2658@users.noreply.github.com>
2021-10-26 06:36:20 +0000

    Add files via upload

--- client.py
+++ client.py
@@ -1,33 +1,102 @@
-# 소켓을 사용하기 위해서는 socket을 import해야 한다.
 import socket
-# 로컬은 ip
-HOST = '192.168.0.250'
-# port는 위 서버에서 설정한 9999로 접속을 한다.
-PORT = 9999
-# 소켓을 만든다.
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# connect함수로 접속을 한다.
-client_socket.connect((HOST, PORT))
-# 10번의 루프로 send receive를 한다.
-for i in range(1,10):
-    # 메시지는 hello로 보낸다.
-    msg = 'hello';
-    # 메시지를 바이너리(byte)형식으로 변환한다.
-    data = msg.encode();
-    # 메시지 길이를 구한다.
-    length = len(data);
-    # server로 리틀 엔디언 형식으로 데이터 길이를 전송한다.
-    client_socket.sendall(length.to_bytes(4, byteorder="little"));
-    # 데이터를 전송한다.
-    client_socket.sendall(data);
-    # server로 부터 전송받을 데이터 길이를 받는다.
-    data = client_socket.recv(4);
-    # 데이터 길이는 리틀 엔디언 형식으로 int를 변환한다.
-    length = int.from_bytes(data, "little");
-    # 데이터 길이를 받는다.
-    data = client_socket.recv(length);
-    # 데이터를 수신한다.
-    msg = data.decode();
-    # 데이터를 출력한다.
-    print('Received from : ', msg);
-client_socket.close();
+from tkinter import filedialog
+from tkinter import messagebox
+import cv2
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import filedialog
+import numpy as np
+########## SS1 Client Test ##########
+# 2021.10.08
+#####################################
+
+class Client_test:
+
+    def send_text(self,ip,port):
+        msg = str(input("Send massage : "))
+        if msg == "stop":
+            return "f"
+        if len(msg) > 12:
+            print("*** ERROR_MESSAGE_TOO_LONG {0}".format(msg))
+            return
+        text = "TEXT" + msg
+        buffer = text.encode()
+        print(buffer)
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # 소켓을 만든다
+        client_socket.connect((ip, port)) # 접속
+        client_socket.sendall(buffer)
+        client_socket.close()
+        return "t"
+
+    def send_file(self,ip,port):
+
+        """
+        files = filedialog.askopenfilenames(initialdir="/Users/jyko/Downloads",
+                                            title="파일을 선택 해 주세요",
+                                            filetypes=(('jpg files','*.jpg'),('png files','*.png')))
+
+        """
+        head = bytearray(16)
+        head[0] = ord('D')
+        head[1] = ord('A')
+        head[2] = ord('T')
+        head[3] = ord('A')
+
+        x = 1024 # x = sizeX
+        b0 = x & 0xff
+        b1 = (x >> 8) & 0xff
+        b2 = (x >> 16) & 0xff
+        b3 = (x >> 24) & 0xff
+        head[4] = b0
+        head[5] = b1
+        head[6] = b2
+        head[7] = b3
+
+        y = 512 # Y = sizeY
+        c0 = y & 0xff
+        c1 = (y >> 8) & 0xff
+        c2 = (y >> 16) & 0xff
+        c3 = (y >> 24) & 0xff
+        head[8] = c0
+        head[9] = c1
+        head[10] = c2
+        head[11] = c3
+
+        """
+        img = cv2.imread(files[0])
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray_1_d = np.ravel(gray) # 1차원 배열
+        body_size = len(gray_1_d)
+        print("body_size: ", body_size)
+        body = bytearray(body_size)
+        for i in range(body_size):
+            body[i] = gray_1_d[i]
+        """
+
+        files = 'cat.jpg'
+        with open(files, "rb") as f:
+            body = f.read()
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # 소켓을 만든다
+        client_socket.connect((ip, port)) # 접속
+        client_socket.sendall(head)
+        client_socket.sendall(body)
+        client_socket.close()
+
+
+
+def main():
+    ip = 'localhost'
+    port = 5001
+    test = Client_test()
+
+    """
+    while True:
+        flag = test.send_text(ip,port)
+        if flag == "f":
+            break
+    """
+    test.send_file(ip,port)
+    #file = test.select_file() # 파일 선택
+
+if __name__ == "__main__":
+	main()
